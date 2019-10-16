aa0b071ecd18547813d8820a13bc6577bf1f2197
Mustakim Patvekar <mustakip@thoughtworks.com>
2019-10-16 12:16:43 +0530

    Reomve old Login component

--- src/__tests__/app/HomePage/HomePage-test.js
+++ src/__tests__/app/HomePage/HomePage-test.js
@@ -2,23 +2,24 @@ import React from 'react';
 import {mount} from "enzyme";
 import HomePage from "../../../app/HomePage/HomePage";
 import PropTypes from "prop-types";
-import Login from "../../../app/components/Login";
 
 describe("HomePage", () => {
+  let component;
+  let router;
+
+  beforeEach(() => {
+    const mockRouteFunc = jest.fn();
+    router = {push: mockRouteFunc};
+    let testContext = {context: {router: router}, childContextTypes: {router: PropTypes.object}};
+    component = mount(<HomePage/>, testContext);
+  });
 
   it('should render Login and signup buttons', function () {
-    const component = mount(<HomePage/>);
-
     expect(component.find('.login-button').props().children).toEqual('Login');
     expect(component.find('.signup-button').props().children).toEqual('Signup');
   });
 
   it('should send to login page on click of login button', function () {
-    const mockRouteFunc = jest.fn();
-    const router = {push: mockRouteFunc};
-    let testContext = {context: {router: router}, childContextTypes: {router: PropTypes.object}};
-    const component = mount(<HomePage/>, testContext);
-
     const loginButton = component.find(".login-button");
     loginButton.simulate('click');
 
@@ -26,11 +27,6 @@ describe("HomePage", () => {
   });
 
   it('should send to signup page on click of signup button', function () {
-    const mockRouteFunc = jest.fn();
-    const router = {push: mockRouteFunc};
-    let testContext = {context: {router: router}, childContextTypes: {router: PropTypes.object}};
-    const component = mount(<HomePage/>, testContext);
-
     const loginButton = component.find(".signup-button");
     loginButton.simulate('click');
 
--- src/__tests__/app/Login/Login-test.js
+++ src/__tests__/app/Login/Login-test.js
@@ -1,49 +1,17 @@
 import React from 'react';
 import {mount} from "enzyme";
 import PropTypes from "prop-types";
-import Login from "../../../app/components/Login";
+import Login from "../../../app/Login/Login";
 import {createStore} from "redux";
 
 describe("Login", () => {
-  let updateUsername;
-  let store;
-
-  beforeEach(() => {
-    updateUsername = jest.fn();
-    store = {
-      ...createStore((x) => x, {username: "username"}),
-      dispatch: jest.fn()
-    };
-  });
-
   it('should render Login component', function () {
-    const component = mount(<Login store={store}/>);
+    const component = mount(<Login />);
+    const loginForm = component.find('.login-form');
 
-    expect(component.find('.login-button').props().children).toEqual('Login');
-    expect(component.find('.username-input-box').props().id).toEqual('username-input');
     expect(component.length).toEqual(1);
-  });
-
-  it('should redirect to Login on click of Login button if username is present in input box', function () {
-    const mockRouteFunc = jest.fn();
-    const router = {push: mockRouteFunc};
-    let testContext = {context: {router: router}, childContextTypes: {router: PropTypes.object}};
-    const username = 'Username';
-    const query = {
-      username: username
-    };
-
-    const DashboardComponent = mount(<Login location={{query: query}}
-                                            updateUsername={jest.fn()}
-                                            store={store}
-    />, testContext);
-    const loginButton = DashboardComponent.find('.login-button');
-    loginButton.simulate('click');
-
-    expect(router.push).toBeCalledWith({
-      pathname: '/chats',
-      query: query
-    });
+    expect(loginForm.find('.username-input').props().name).toEqual('username');
+    expect(loginForm.find('.password-input').props().name).toEqual('password');
   });
 });
 
